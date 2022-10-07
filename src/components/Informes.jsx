6bbc7c434d5e43b8aba8744d0237d978e1c392a2
unknown <jerez4959@gmail.com>
2022-10-07 12:53:42 -0300

    first committttttffta

--- src/components/Informes.jsx
+++ src/components/Informes.jsx
@@ -42,12 +42,36 @@ const Informes = () => {
                                 <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{paciente.suma}</td>                                    
                             </tr>
                             ))}
+                            {remas.map( (useRema) => (
+                            <tr key={useRema._id} className='dark:bg-gray-100  hover:bg-lime-600 uppercase'>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap ">{useRema.tipo}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.cliente}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.numeroOc}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.producto}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.numero1.uno}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.numero1.dos}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.suma}</td>                                    
+                            </tr>
+                            ))}
+                            
+                            {cols.map( (col) => (
+
+                            <tr key={col._id} className='dark:bg-gray-100  hover:bg-lime-600 uppercase'>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap ">{col.tipo}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.cliente}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.numeroOc}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.producto}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.numero1.uno}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.numero1.dos}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.suma}</td>                                    
+                            </tr>
+                            ))}
                         </tbody>
                         
                     </table>
 
 
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    {/* <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-400">
                                
                             </thead>
@@ -70,6 +94,19 @@ const Informes = () => {
                                 <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.numero1.uno}</td>
                                 <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.numero1.dos}</td>
                                 <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.suma}</td>                                    
+                            </tr>
+                            ))}
+
+                            {cols.map( (col) => (
+
+                            <tr key={col._id} className='dark:bg-gray-100  hover:bg-lime-600 uppercase'>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap ">{col.tipo}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.cliente}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.numeroOc}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.producto}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.numero1.uno}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.numero1.dos}</td>
+                                <td className="w-1/7 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.suma}</td>                                    
                             </tr>
                             ))}
                         </tbody>
@@ -105,7 +142,10 @@ const Informes = () => {
                             ))}
                         </tbody>
                         
-                    </table>
+                    </table> */}
+
+
+                    
                 </div>
         </div>       
 
