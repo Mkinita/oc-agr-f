efbb3ea79478ef5ba44d0415632e6478ce4429bf
unknown <jerez4959@gmail.com>
2022-10-07 12:21:35 -0300

    first committttttt

--- src/components/Informes.jsx
+++ src/components/Informes.jsx
@@ -8,116 +8,89 @@ const Informes = () => {
     const {cols} = useCol()
   return (
     
-    <>
+    <>          
+            
               <img className="md:0 md:mx-auto border-8 border-transparent" src="/img/logo.jpg"/>
+              <h2 className='font-black text-3xl text-center border-8 border-transparent'>Listado de Ordenes de Compras</h2>
+
+  
+           
+        <div  class="text-center">
+                <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-400">
+                                <tr>
+                                <th scope="col" className="w-1/6 p-2 text-center text-xs text-black">Tipo</th>
+                                <th  scope="col" className="w-1/6 p-2 text-center text-xs text-black">Cliente</th>
+                                <th  scope="col" className="w-1/6 p-2 text-center text-xs text-black">OC</th>
+                                <th  scope="col" className="w-1/6 p-2 text-center text-xs text-black">Producto</th>
+                                <th  scope="col" className="w-1/6 p-2 text-center text-xs text-black">Solic.</th>
+                                <th  scope="col" className="w-1/6 p-2 text-center text-xs text-black">Desap.</th>
+                                <th  scope="col" className="w-1/6 p-2 text-center text-xs text-black">saldo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {pacientes.map( (paciente) => (
+
+                            <tr key={paciente._id} className='dark:bg-gray-100  hover:bg-lime-600 uppercase'>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap ">{paciente.tipo}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{paciente.cliente}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{paciente.numeroOc}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{paciente.producto}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{paciente.numero1.uno}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{paciente.numero1.dos}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{paciente.suma}</td>                                    
+                            </tr>
+                            ))}
+                        </tbody>
+                        
+                    </table>
 
 
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-400">
+                               
+                            </thead>
+                            <tbody>
+                            {remas.map( (useRema) => (
+                            <tr key={useRema._id} className='dark:bg-gray-100  hover:bg-lime-600 uppercase'>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap ">{useRema.tipo}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.cliente}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.numeroOc}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.producto}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.numero1.uno}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.numero1.dos}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{useRema.suma}</td>                                    
+                            </tr>
+                            ))}
+                        </tbody>
 
+                        
+                        
+                    </table>
 
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-400">
+                            </thead>
+                            <tbody>
+                            {cols.map( (col) => (
 
-              
-              <h2 className='font-black text-3xl text-center'>Listado de Ordenes de Compras</h2>
-              
-    <div className='p-10'>
-    <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-400">
-                        <tr>
-                        <th scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase">Tipo</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ">Cliente</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ">N° OC</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ">Producto</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ">Solicitados</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ">Desapacho</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ">saldo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {pacientes.map( (paciente) => (
+                            <tr key={col._id} className='dark:bg-gray-100  hover:bg-lime-600 uppercase'>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap ">{col.tipo}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.cliente}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.numeroOc}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.producto}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.numero1.uno}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.numero1.dos}</td>
+                                <td className="w-1/6 p-2 text-center text-xs text-gray-800 whitespace-nowrap">{col.suma}</td>                                    
+                            </tr>
+                            ))}
+                        </tbody>
+                        
+                    </table>
+                </div>
+        </div>       
 
-                    <tr key={paciente._id} className='dark:bg-gray-100  hover:bg-lime-600 uppercase'>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap ">{paciente.tipo}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{paciente.cliente}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{paciente.numeroOc}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{paciente.producto}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{paciente.numero1.uno}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{paciente.numero1.dos}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{paciente.suma}</td>                                    
-                    </tr>
-                    ))}
-                </tbody>
-                
-            </table>
-            
-            
-        </div>
-        <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-400">
-                        <tr>
-                        <th scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase">Tipo</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">Cliente</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">N° OC</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">Producto</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">Solicitados</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">Desapacho</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">saldo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {remas.map( (rema) => (
-
-                    <tr key={rema._id} className='dark:bg-gray-100  hover:bg-lime-600 uppercase'>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap ">{rema.tipo}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{rema.cliente}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{rema.numeroOc}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{rema.producto}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{rema.numero1.uno}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{rema.numero1.dos}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{rema.suma}</td>                                    
-                    </tr>
-                    ))}
-                </tbody>
-                
-            </table>
-            
-            
-        </div>
-
-
-        <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-400">
-                        <tr>
-                        <th scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase">Tipo</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">Cliente</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">N° OC</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">Producto</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">Solicitados</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">Desapacho</th>
-                        <th  scope="col" className="px-6 py-3 text-xs font-bold text-left text-gray-200 uppercase ">saldo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {cols.map( (col) => (
-
-                    <tr key={col._id} className='dark:bg-gray-100  hover:bg-lime-600 uppercase'>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap ">{col.tipo}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{col.cliente}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{col.numeroOc}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{col.producto}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{col.numero1.uno}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{col.numero1.dos}</td>
-                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">{col.suma}</td>                                    
-                    </tr>
-                    ))}
-                </tbody>
-                
-            </table>
-            
-            
-        </div>
-        </div>
         
     </>
   )
